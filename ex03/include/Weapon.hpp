a497fc5387a60a8ba40eb8c6869b9944a96faa8c
etienne-hd <hode.etienne@gmail.com>
2025-11-25 15:53:47 +0100

    Changed: weapon is NULL by default in HumanB

--- ex03/include/Weapon.hpp
+++ ex03/include/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 02:18:00 by ehode             #+#    #+#             */
-/*   Updated: 2025/11/25 02:47:04 by ehode            ###   ########.fr       */
+/*   Updated: 2025/11/25 15:50:25 by ehode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ class Weapon
 	private:
 		std::string type;
 	public:
-		Weapon(void);
 		Weapon(std::string type);
 		~Weapon(void);
 		const std::string &getType(void);
--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 02:29:53 by ehode             #+#    #+#             */
-/*   Updated: 2025/11/25 02:57:37 by ehode            ###   ########.fr       */
+/*   Updated: 2025/11/25 15:52:13 by ehode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 HumanB::HumanB(std::string name)
 {
+	this->weapon = NULL;
 	this->name = name;
 }
 
@@ -22,7 +23,10 @@ HumanB::~HumanB(void) {}
 
 void HumanB::attack(void)
 {
-	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	if (this->weapon)
+		std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	else
+		std::cout << this->name << " attacks with their hands" << std::endl;
 }
 
 void HumanB::setWeapon(Weapon &weapon)
