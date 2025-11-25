e0c74acfdc239d49b9b86f436dabf3a845a9fab7
etienne-hd <hode.etienne@gmail.com>
2025-11-25 02:15:25 +0100

    Added: ex02

---
+++ ex02/src/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/24 19:41:21 by ehode             #+#    #+#             */
+/*   Updated: 2025/11/25 02:15:20 by ehode            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	// Address
+	std::cout << "Address of str: " << &str << std::endl;
+	std::cout << "Address of stringPTR: " << &stringPTR << std::endl;
+	std::cout << "Address of stringREF: " << &stringREF << std::endl;
+
+	// Value pointed by address
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+}
